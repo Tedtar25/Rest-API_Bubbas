2b15bb9f58b223515c5e9047061e53a889bd57e7
Tedtar25 <cesar.carrillo69@hotmail.com>
2025-11-11 22:13:08 -0600

    Rutas de productos y aniadido en rutas generales

--- routes/productoRoutes.js
+++ routes/productoRoutes.js
@@ -1,32 +1,82 @@
-const express = require('express')
-const productoService = require('../services/productoService');
-const { route } = require('./pedidoRoutes');
+import express, { json } from 'express';
+import productoService from '../services/productoService.js';
 
 const router = express.Router();
 
 //Obtener todos los productos
-router.get('api/productos', (req, res)=>{
-
+router.get('/', async (req, res, next) => {
+    try {
+        const products = await productoService.getAll();
+        res.json(products)
+    } catch (error) {
+        next(error)
+    }
 });
 
-//Obtener un producto por su ID
-router.get('api/productos/:id',(req, res)=>{
-
+//Obtener producto por ID
+router.get('/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const products = await productoService.getById(id);
+        if (products) {
+            res.status(200).json(products);
+        } else {
+            res.status(404), json({ message: ' Producto no encontrado' })
+        }
+    } catch (error) {
+        next(error)
+    }
 });
 
-//Agregar un nuevo producto
-router.post('api/productos',(req, res)=>{
-
+//Agregar nuevo producto
+router.post('/', async (req, res, next) => {
+    try {
+        const { nombre, descripcion, precio, disponible, categoria } = req.body;
+        const newProduct = await productoService.create({ nombre, descripcion, precio, disponible, categoria });
+        res.status(201).json({
+            message: 'Producto agregado',
+            data: newProduct
+        })
+    } catch (error) {
+        next(error)
+    }
 });
 
-//Modificar si un producto esta disponible
-router.put('api/productos/:id', (req, res)=>{
-
-});
+//Modificar los datos de un producto
+router.patch('/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { precio, disponible, categoria } = req.body;
+        const updateProduct = await productoService.update(id, { precio, disponible, categoria });
+        if (updateProduct) {
+            res.status(200).json({
+                message: 'Producto actualizado',
+                data: updateProduct
+            });
+        } else {
+            res.status(404).json({ message: 'Producto no encontrado' })
+        }
+    } catch (error) {
+        next(error)
+    }
+})
 
 //Eliminar un producto
-router.delete('api/productos/:id',(req, res)=>{
+router.delete('/:id', async (req, res, next)=>{
+    try{
+        const {id} = req.params;
+        const deletedProduct = await productoService.delete(id);
+        if(deletedProduct){
+            res.status(200).json({
+                message: 'Producto eliminado',
+                data:deletedProduct
+            })
+        }else{
+            res.status(404).json({message: 'Producto no encontrado'})
+        }
+    }catch(error){
+        next(error)
+    }
+})
 
-});
-
-module.exports = router;
+export default router;
--- routes/rutasGeneral.js
+++ routes/rutasGeneral.js
@@ -1,7 +1,9 @@
 import usuarioRouter from '../routes/usuarioRoutes.js';
+import productoRouter from '../routes/productoRoutes.js';
 
 function routerApi(app) {
   app.use('/v1/usuarios', usuarioRouter);
+  app.use('/v1/productos', productoRouter)
 }
 
 export default routerApi;
