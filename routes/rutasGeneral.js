5f35d41df200d8f97fd3d5853b2dcd701f519608
Tedtar25 <cesar.carrillo69@hotmail.com>
2025-11-18 14:57:24 -0600

    Agregacion de la ruta para los pedidos completos

--- routes/rutasGeneral.js
+++ routes/rutasGeneral.js
@@ -3,11 +3,23 @@ import productoRouter from '../routes/productoRoutes.js';
 import pedidoRouter from '../routes/pedidoRoutes.js';
 import detallePedidoRouter from '../routes/detallePedidoRoutes.js'
 
+import express from express;
+
+const router = express.router();
+
 function routerApi(app) {
   app.use('/v1/usuarios', usuarioRouter);
   app.use('/v1/productos', productoRouter);
   app.use('/v1/pedidos', pedidoRouter);
   app.use('/v1/detalle_Pedido', detallePedidoRouter)
 }
+
+router.post('/v1/nuevo',async (req, res, next) =>{
+  try{
+    
+  }catch(error){
+    next(error)
+  }
+})
 
 export default routerApi;
